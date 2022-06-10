85b8c98d4f751894a0dbaa62c96fd0ff7b2a563f
Paweł Szmergała <pawelszme@gmail.com>
2022-06-10 14:48:20 +0200

    Implement computing distance between every pair of fields

--- pacman/engine.py
+++ pacman/engine.py
@@ -1,8 +1,10 @@
-from numpy import array, float64, zeros, zeros_like
+from numpy import array, float64, zeros, zeros_like, inf
 from numpy.random import choice
 from math import  floor, ceil
 from itertools import product
 from pacman import settings
+from tqdm.auto import tqdm
+from typing import Dict, Tuple
 
 
 
@@ -37,13 +39,13 @@ class Engine:
 		"""
 		world = open(file).read().split('\n')
 		width, height = len(world[0]), len(world)
-		self.score = self.victory_score = self.time = 0		
+		self.score = self.victory_score = self.time = 0
 		self.agents = [pacman] + ghosts
 		self.pacman = pacman
 		self.channels = zeros(shape = (4, height, width), dtype = int)
 		self.shape = array((width, height))
 		self.walls, self.coins, self.ghosts, self.distances = self.channels
-		
+
 		for x, y in product(range(width), range(height)):
 			cell = world[y][x]
 			if cell == Engine.Wall:
@@ -64,6 +66,44 @@ class Engine:
 				ghost = self.agents[gid]
 				ghost.color = settings.ghosts[(gid - 1) % len(settings.ghosts)]
 				ghost.position, ghost.walls = array((x, y), dtype=float64), self.walls
+
+		self.dist : Dict[Tuple[int, int, int, int], int] = {}
+		"""Stores distance between every pair of fields. Usage: `self.dist[(from_x, from_y, to_x, to_y)]`."""
+		for from_x, from_y in product(range(width), range(height)):
+			for to_x, to_y in product(range(width), range(height)):
+				if from_x == to_x and from_y == to_y:
+					self.dist[(from_x, from_y, to_x, to_y)] = 0
+				# Walls are unreachable.
+				elif world[from_y][from_x] == Engine.Wall or world[to_y][to_x] == Engine.Wall:
+					self.dist[(from_x, from_y, to_x, to_y)] = inf
+				# Difference by one vertically/horizontally.
+				elif (abs(from_x - to_x) == 1 and abs(from_y - to_y) == 0) or (abs(from_x - to_x) == 0 and abs(from_y - to_y) == 1):
+					self.dist[(from_x, from_y, to_x, to_y)] = 1
+				else:
+					self.dist[(from_x, from_y, to_x, to_y)] = inf
+		# Special case - corresponding fields near the opposite edges that allow warping.
+		for x in range(width):
+			if world[0][x] != Engine.Wall and world[height - 1][x] != Engine.Wall:
+				self.dist[(x, 0, x, height - 1)] = 1
+				self.dist[(x, height - 1, x, 0)] = 1
+		for y in range(height):
+			if world[y][0] != Engine.Wall and world[y][width - 1] != Engine.Wall:
+				self.dist[(0, y, width - 1, y)] = 1
+				self.dist[(width - 1, y, 0, y)] = 1
+		self.dist[(0, 12, 28, 12)] = 1
+		self.dist[(28, 12, 0, 12)] = 1
+		# Compute distance between each pair of board fields using Floyd-Warshall algorithm.
+		coordinates = []
+		for x, y in product(range(width), range(height)):
+			# Do not consider walls in the algorithm.
+			if world[y][x] != Engine.Wall:
+				coordinates.append((x, y))
+		for mid_x, mid_y in tqdm(coordinates):
+			for from_x, from_y in coordinates:
+				for to_x, to_y in coordinates:
+					dist_through_mid = self.dist[(from_x, from_y, mid_x, mid_y)] + self.dist[(mid_x, mid_y, to_x, to_y)]
+					if self.dist[(from_x, from_y, to_x, to_y)] > dist_through_mid:
+						self.dist[(from_x, from_y, to_x, to_y)] = dist_through_mid
 
 
 	def __next__(self):
@@ -121,13 +161,13 @@ class Agent:
 	Entity capable of interacting with the game engine.
 	Use this template to create various game agents through subclassing.
 	Note that __call__( ) method is virtual and has to be implemented ( see RandomAgent example ).
-	
+
 	Attributes:
 		speed		-- the speed with which the agent traverses the environment
 
 		position	-- real agent position ( for discrete representation use Agent.coords( ) instead )
 	"""
-	
+
 	L = array((-1, 0))
 	R = array(( 1, 0))
 	U = array(( 0,-1))
