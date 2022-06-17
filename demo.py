1db1e3cfa41ce90ea6d24f9f5df815146fd8bd9d
Ventus <ventuss550@gmail.com>
2022-06-17 17:49:25 +0200

    fix demo

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 from pacman import GameController, RandomAgent
 
 
-pacman = RandomAgent()
-ghosts = [RandomAgent(0.05) for _ in range(3)]
-GameController(pacman, ghosts, file = "pacman/default.map")
+pacman = RandomAgent(0.05)
+ghosts = [RandomAgent(0.025) for _ in range(3)]
+GameController(pacman, ghosts, file = "pacman/default.map").run()
